deebf05629919c04421112bcf21f5bbaf2565f90
Soni <sonikm443@gmail.com>
2023-03-18 20:24:23 +0530

    Added Search functionality

--- main.js
+++ main.js
@@ -94,3 +94,168 @@ function showMore (){
     let more = moreImages.classList.contains('more-image');
     moreBtn.innerHTML = more ? "SHOW LESS" : "SHOW MORE";
 }
+
+// || SEARCHING FUNCTIONALITY OF IMAGES (for search btn1)
+function searchImage(){
+   let image = document.querySelector('#search-text').value;
+   let type = image.toLowerCase();
+
+   if(type == 'animal' || type == 'animals')
+   {
+       window.location.href = './Image_websites/animals.html';
+   }
+   if(type == 'children' || type == 'childrens')
+   {   
+       window.location.href = './Image_websites/childrens.html';
+   }
+   if(type == 'color' || type == 'colors')
+   {   
+       window.location.href = './Image_websites/colors.html';
+   }
+   if(type == 'conifer' || type == 'conifers')
+   {   
+       window.location.href = './Image_websites/conifer.html';
+   }
+   if(type == 'fastion' || type == 'fastions')
+   {   
+       window.location.href = './Image_websites/fastion.html';
+   }
+   if(type == 'flower' || type == 'flowers')
+   {   
+       window.location.href = './Image_websites/flower.html';
+   }
+   if(type == 'food' || type == 'foods')
+   {   
+       window.location.href = './Image_websites/food.html';
+   }
+   if(type == 'fastival' || type == 'fastivals')
+   {   
+       window.location.href = './Image_websites/fastivals.html';
+   }
+   if(type == 'house' || type == 'houses')
+   {   
+       window.location.href = './Image_websites/house.html';
+   }
+   if(type == 'model' || type == 'models')
+   {   
+       window.location.href = './Image_websites/model.html';
+   }
+   if(type == 'mountain' || type == 'mountains')
+   {   
+       window.location.href = './Image_websites/mountains.html';
+   }
+   if(type == 'nature' || type == 'natures')
+   {   
+       window.location.href = './Image_websites/nature.html';
+   }
+   if(type == 'people' || type == 'peoples')
+   {   
+       window.location.href = './Image_websites/people.html';
+   }
+   if(type == 'traveling' || type == 'travelings')
+   {   
+       window.location.href = './Image_websites/travelings.html';
+   }
+   if(type == 'tree' || type == 'trees')
+   {   
+       window.location.href = './Image_websites/trees.html';
+   }
+   if(type == 'vehicle' || type == 'vehicles')
+   {   
+       window.location.href = './Image_websites/vehicles.html';
+   }
+   if(type == 'vintage' || type == 'vintages')
+   {   
+       window.location.href = './Image_websites/vintage.html';
+   }
+   if(type == 'workspace' || type == 'workspaces')
+   {   
+       window.location.href = './Image_websites/workspace.html';
+   }
+   else{
+    alert('not found...')
+   }
+}
+
+
+// || SEARCHING FUNCTIONALITY OF IMAGES (for search btn2)
+function searchImage2(){
+    let image = document.querySelector('#search-text2').value;
+    let type = image.toLowerCase();
+ 
+    if(type == 'animal' || type == 'animals')
+    {
+        window.location.href = './Image_websites/animals.html';
+    }
+    if(type == 'children' || type == 'childrens')
+    {   
+        window.location.href = './Image_websites/childrens.html';
+    }
+    if(type == 'color' || type == 'colors')
+    {   
+        window.location.href = './Image_websites/colors.html';
+    }
+    if(type == 'conifer' || type == 'conifers')
+    {   
+        window.location.href = './Image_websites/conifer.html';
+    }
+    if(type == 'fastion' || type == 'fastions')
+    {   
+        window.location.href = './Image_websites/fastion.html';
+    }
+    if(type == 'flower' || type == 'flowers')
+    {   
+        window.location.href = './Image_websites/flower.html';
+    }
+    if(type == 'food' || type == 'foods')
+    {   
+        window.location.href = './Image_websites/food.html';
+    }
+    if(type == 'fastival' || type == 'fastivals')
+    {   
+        window.location.href = './Image_websites/fastivals.html';
+    }
+    if(type == 'house' || type == 'houses')
+    {   
+        window.location.href = './Image_websites/house.html';
+    }
+    if(type == 'model' || type == 'models')
+    {   
+        window.location.href = './Image_websites/model.html';
+    }
+    if(type == 'mountain' || type == 'mountains')
+    {   
+        window.location.href = './Image_websites/mountains.html';
+    }
+    if(type == 'nature' || type == 'natures')
+    {   
+        window.location.href = './Image_websites/nature.html';
+    }
+    if(type == 'people' || type == 'peoples')
+    {   
+        window.location.href = './Image_websites/people.html';
+    }
+    if(type == 'traveling' || type == 'travelings')
+    {   
+        window.location.href = './Image_websites/travelings.html';
+    }
+    if(type == 'tree' || type == 'trees')
+    {   
+        window.location.href = './Image_websites/trees.html';
+    }
+    if(type == 'vehicle' || type == 'vehicles')
+    {   
+        window.location.href = './Image_websites/vehicles.html';
+    }
+    if(type == 'vintage' || type == 'vintages')
+    {   
+        window.location.href = './Image_websites/vintage.html';
+    }
+    if(type == 'workspace' || type == 'workspaces')
+    {   
+        window.location.href = './Image_websites/workspace.html';
+    }
+    else{
+        alert('not found...')
+       }
+}
